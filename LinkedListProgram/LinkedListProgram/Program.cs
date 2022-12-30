8e12e0e7b81fe144631492672b4f03568be43202
qismat <qismatnisha6243@gmail.com>
2022-12-30 18:23:58 +0530

    [qismat] Add : UC8-InsertElmAtParticularPosition

--- LinkedListProgram/LinkedListProgram/Program.cs
+++ LinkedListProgram/LinkedListProgram/Program.cs
@@ -9,12 +9,11 @@
             LinkedList linkdedList = new LinkedList();
             linkdedList.Add(56);
             linkdedList.Add(30);
+            linkdedList.Add(40);
             linkdedList.Add(70);
             linkdedList.Display();
-            //linkdedList.DeleteLastNode();
-            // linkdedList.Display();
-            //linkdedList.Search(30);
-            linkdedList.InsertAtParticularPosition(3, 40);
+
+            linkdedList.deleteAtParticularPosition(3, 40);
             linkdedList.Display();
         }
     }
