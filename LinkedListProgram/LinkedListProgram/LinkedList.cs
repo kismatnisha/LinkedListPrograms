4a05e3657a200106f90b10a2eb75320ae80d1d78
qismat <qismatnisha6243@gmail.com>
2022-12-30 15:53:17 +0530

    [qismat] Add : UC3-AppendingMethod

--- LinkedListProgram/LinkedListProgram/LinkedList.cs
+++ LinkedListProgram/LinkedListProgram/LinkedList.cs
@@ -9,49 +9,46 @@ namespace LinkedListProgram
   
         internal class LinkedList
         {
-            internal Node head;
-            internal void AddLast(int data)
+        internal Node head;
+        internal void Add(int data)
+        {
+            Node Node = new Node(data);
+            if (this.head == null)
             {
-                Node Node = new Node(data);
-                if (this.head == null)
-                {
-                    this.head = Node;
-                }
-                else
-                {
-                    Node temp = this.head;
-                    while (temp.next != null)
-                    {
-                        temp = temp.next;
-                    }
-                    temp.next = Node;
-                }
-                Console.WriteLine("{0} is inserted into LinkedList", Node.data);
-            }
-            internal void AddFirst(int data)
-            {
-                Node Node = new Node(data);
-                Node.next = this.head;
                 this.head = Node;
-                Console.WriteLine("{0} is inserted into LinkedList", Node.data);
             }
-            public void Display()
+            else
             {
-                Console.WriteLine("Displaying Nodes:");
                 Node temp = this.head;
-                if (temp == null)
+                while (temp.next != null)
                 {
-                    Console.WriteLine("LinkedList is Empty");
-                    return;
+                    temp = temp.next;
                 }
-                else
+                temp.next = Node;
+            }
+            Console.WriteLine("{0} is inserted into LinkedList", Node.data);
+        }
+        internal void Append(int data)
+        {
+            Add(data);
+        }
+        public void Display()
+        {
+            Console.WriteLine("Displaying Nodes:");
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is Empty");
+                return;
+            }
+            else
+            {
+                while (temp != null)
                 {
-                    while (temp != null)
-                    {
-                        Console.Write(" " + temp.data + " ");
-                        temp = temp.next;
-                    }
+                    Console.Write(" " + temp.data + " ");
+                    temp = temp.next;
                 }
             }
         }
     }
+}
